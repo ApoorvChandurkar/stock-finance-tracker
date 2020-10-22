21179d8bbed9cd2558eba700feb4fb04cec3d1ad
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-22 16:45:38 +0530

    added navigation option to edit profile and changed user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def count_under_limit?
-      stocks.count < 10
+    stocks.count < 10
   end
 
   def already_tracking?(ticker_symbol)
@@ -20,4 +20,8 @@ class User < ApplicationRecord
     count_under_limit? && !already_tracking?(ticker_symbol)
   end
 
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
 end
