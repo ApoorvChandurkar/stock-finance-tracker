932d00077cf5f7289f836c5de0e08c9a35dbd2b6
Apoorv Chandurkar <apoorvchandurkar@gmail.com>
2020-10-22 12:49:19 +0530

    added restrictions to the portfolio add feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,19 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def count_under_limit?
+      stocks.count < 10
+  end
+
+  def already_tracking?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false if stock.nil?
+    
+    !stocks.find_by(ticker: ticker_symbol).nil?
+  end
+  def can_track_stock?(ticker_symbol)
+    count_under_limit? && !already_tracking?(ticker_symbol)
+  end
+
 end
